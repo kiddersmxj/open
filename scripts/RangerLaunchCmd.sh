e29427c38bac7cc2bf4a582f9101760764f8a4cd
kiddersmxj <maxkiddmay@gmail.com>
2025-05-07 19:29:57 +0100

    fixed scripts to work with ble.sh interactivity

--- scripts/RangerLaunchCmd.sh
+++ scripts/RangerLaunchCmd.sh
@@ -1,6 +1,23 @@
-#!/bin/sh -i
+#!/bin/sh
 
 source ~/.bashrc
 cd "$@"
+
+logdatetimeFunc() {
+    printf $(date '+%Y-%m-%d[%H:%M:%S]')
+}
+
+r() {
+    temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
+    # Dynamically generate the log file path with the current datetime
+    log_file="/log/ranger/$(logdatetimeFunc).ranger.log"
+    python ~/git/cli/ranger/ranger.py --choosedir="$temp_file" --logfile "$log_file" -- "${@:-$PWD}"
+    if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
+        cd -- "$chosen_dir"
+    fi
+    rm -f -- "$temp_file"
+}
+
 "r"
+
 exec "$SHELL"
